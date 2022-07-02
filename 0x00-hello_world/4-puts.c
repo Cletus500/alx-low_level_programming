b0a2b07054f8ab450ff50946d44ce7765d1944ec
Busayo Ologunleko <ologunlekocletus@gmail.com>
2022-07-01 18:35:48 -0700

    Hello. puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - prints "Programming is like building a multilingual puzzle
  *
  * Return: Always 0
- *
  */
+
 int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 
 	Return (0);
-}
