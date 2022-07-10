3525f933213560cfa98a8cd202ecf2f326102c53
Busayo Ologunleko <ologunlekocletus@gmail.com>
2022-07-09 17:40:29 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,33 +7,28 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 	{
-		while (n <= 98)
+		for (; n <= 98; n++)
 		{
 			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
+
+			if (n == 98)
+				continue;
+			printf(", ");
 		}
-	}
-	else if (n > 98)
-	{
-		while (n >= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n--;
-		}
+		printf("\n");
 	}
 	else
 	{
-		printf("98");
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
 	}
-	printf("\n");
 }
