9b3b0e399a6142442a935ba436a4d5d09ac17831
Busayo Ologunleko <ologunlekocletus@gmail.com>
2022-07-14 13:56:56 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,21 +5,19 @@
  * @str: string that will be encoded
  * Return: returns encoded string
  */
-
 char *leet(char *str)
 {
-	int i = 0, j;
-	char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
-	     str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int index1= 0, index2;
+	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (str[i])
+	while (str[++index1])
 	{
-		for (j = 0; j < 10; j++)
-			if (str[i] == str[j])
-				str[i] = subs[j];
-
-		i++;
+		for (index2 = 0; index2 <= 7; index2++)
+		{
+			if (str[index1] == leet[index2] ||
+					str[index1] - 32 == leet[index2])
+				str[index1] = index2 + '0';
+		}
 	}
-
 	return (str);
 }
