aad514d71a8b6e9e0011ff0eb27067dbf167c4a0
Busayo Ologunleko <ologunlekocletus@gmail.com>
2022-07-14 09:19:50 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,16 +4,18 @@
  * _strcat - a function that concatenates two strings.
  * @dest: an input string
  * @src: an input string
- * Return: A pointer to @dest
+ * Return: A pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
-	while (dest[index++])
-		dest_len++;
+	char *temp = dest;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (*dest)
+		dest++;
 
-	return (dest);
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
 }
