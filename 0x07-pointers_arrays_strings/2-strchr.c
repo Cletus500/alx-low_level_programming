ee7290bb6991146f23a080f543987f5ac8b371fb
Busayo Ologunleko <ologunlekocletus@gmail.com>
2022-07-18 12:05:54 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,12 +8,9 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] >= '\0'; i++)
+	while (c != *s)
 	{
-		if (s[i] == c)
-			return (s + i);
+		s++;
 	}
-	return (NULL);
+	return (s);
 }
