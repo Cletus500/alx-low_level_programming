97518d563e1637093c58d02b79ca90477e059ae3
Busayo Ologunleko <ologunlekocletus@gmail.com>
2022-07-19 17:07:13 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,10 +5,14 @@
  * @s: input
  * Return: Always 0 (Success)
  */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-		return;
-	_print_rev_recursion(s + 1);
+		_putchar('\n');
+	else
+	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
+	}
 }
